9edf27ff5c1803ce4bbaf845ccd4ef6588678d3c
TinDoma <christinedomatol@gmail.com>
2026-06-23 14:49:06 +0800

    remove .env

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,6 @@ import 'package:StarSight/business_layer/orientation_service.dart';
 import 'package:StarSight/ui_layer/splash_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
-import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -11,7 +10,6 @@ void main() async {
 
   await Future.wait([Firebase.initializeApp()]);
 
-  await dotenv.load(fileName: ".env");
   runApp(const App());
 }
 
@@ -20,6 +18,9 @@ class App extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(debugShowCheckedModeBanner: false, home: SplashScreen());
+    return MaterialApp(
+        debugShowCheckedModeBanner: false,
+        home: SplashScreen()
+    );
   }
 }
